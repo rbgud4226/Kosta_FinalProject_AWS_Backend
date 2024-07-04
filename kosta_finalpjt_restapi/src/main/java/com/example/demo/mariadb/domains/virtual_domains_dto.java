389d27aa85813befccd3d76c24a259d8377907c5
choiwanmin <choi.wm512@gmail.com>
2024-07-04 11:52:38 +0900

    refactor:메일 서버 비활성화 주석 처리

--- kosta_finalpjt_restapi/src/main/java/com/example/demo/mariadb/domains/virtual_domains_dto.java
+++ kosta_finalpjt_restapi/src/main/java/com/example/demo/mariadb/domains/virtual_domains_dto.java
@@ -1,13 +1,13 @@
-package com.example.demo.mariadb.domains;
-
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-public class virtual_domains_dto {
-  private int id;
-  private String name;
-}
+//package com.example.demo.mariadb.domains;
+//
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
+//public class virtual_domains_dto {
+//  private int id;
+//  private String name;
+//}
--- kosta_finalpjt_restapi/src/main/java/com/example/demo/mariadb/users/virtual_users_dto.java
+++ kosta_finalpjt_restapi/src/main/java/com/example/demo/mariadb/users/virtual_users_dto.java
@@ -1,16 +1,16 @@
-package com.example.demo.mariadb.users;
-
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-public class virtual_users_dto {
-  private int id;
-  private int domainId;
-  private String password;
-  private String email;
-  private String box;
-}
+//package com.example.demo.mariadb.users;
+//
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
+//public class virtual_users_dto {
+//  private int id;
+//  private int domainId;
+//  private String password;
+//  private String email;
+//  private String box;
+//}
