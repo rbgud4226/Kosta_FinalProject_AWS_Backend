cf9b05ea6c09b6690d9f20bde487ee61f94bf8d3
rbgud4226 <hkh202857@gmail.com>
2024-07-09 17:05:20 +0900

    refactor:스프링뱃지 스케쥴러 코드 수정

--- kosta_finalpjt_restapi/src/main/java/com/example/demo/oracledb/chat/Manager/BatchConfig.java
+++ kosta_finalpjt_restapi/src/main/java/com/example/demo/oracledb/chat/Manager/BatchConfig.java
@@ -1,10 +1,14 @@
 package com.example.demo.oracledb.chat.Manager;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
@@ -17,13 +21,18 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.demo.oracledb.chat.Room.ChatRoom;
 import com.example.demo.oracledb.chat.Room.ChatRoomService;
 import com.example.demo.oracledb.chat.RoomUser.RoomUserService;
+import org.springframework.batch.core.launch.JobLauncher;
 
 import jakarta.transaction.Transactional;
 
@@ -40,9 +49,11 @@ public class BatchConfig extends DefaultBatchConfiguration {
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
+    @Lazy
     private ChatRoomService chatRoomService;
 
     @Autowired
+    @Lazy
     private RoomUserService roomUserService;
 
     @Autowired
@@ -81,7 +92,7 @@ public class BatchConfig extends DefaultBatchConfiguration {
 
     @Transactional
     private void deleteOldMessages() {
-        String sql = "DELETE FROM MESSAGE WHERE TO_TIMESTAMP(SENDDATE, 'YYYY\"년 \"MM\"월 \"DD\"일 \"HH24:MI:SS') < SYSDATE - INTERVAL '3' MONTH";
+    	String sql = "DELETE FROM MESSAGE WHERE sendDate < SYSDATE - INTERVAL '3' MONTH";
         jdbcTemplate.execute(sql);
     }
     
@@ -96,5 +107,41 @@ public class BatchConfig extends DefaultBatchConfiguration {
             }
         }
         chatRoomService.delChatRoomBychatroomid();
+    }
+    
+    //spring.batch.jdbc.initialize-schema=always 용도
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+      DataSourceInitializer initializer = new DataSourceInitializer();
+      initializer.setDataSource(dataSource);
+      ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+      populator.addScript(new ClassPathResource("org/springframework/batch/core/schema-oracle.sql"));
+      initializer.setDatabasePopulator(populator);
+      try (Connection connection = dataSource.getConnection();
+           Statement statement = connection.createStatement()) {
+          statement.execute("SELECT 1 FROM BATCH_JOB_INSTANCE WHERE 1=0");
+          initializer.setEnabled(false);
+      } catch (SQLException e) {
+          initializer.setEnabled(true);
+      }
+
+      return initializer;
+    }
+    
+    @Configuration
+    public static class SchedulerConfig {
+
+        @Autowired
+        private JobLauncher jobLauncher;
+
+        @Autowired
+        private Job chatManageJob;
+
+        @Scheduled(fixedRate = 6000000)
+        public void perform() throws Exception {
+            JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+            jobParametersBuilder.addLong("time", System.currentTimeMillis());
+            jobLauncher.run(chatManageJob, jobParametersBuilder.toJobParameters());
+        }
     }
 }
